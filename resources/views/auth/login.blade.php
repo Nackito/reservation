e7690b4e2fa779ad2c03454e4f488d814313540f
Nackito <nackfrank@gmail.com>
2025-08-03 14:54:42 +0200

    Remplacement du bouton de connexion par un élément de bouton personnalisé avec styles améliorés sur la page login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,9 +56,9 @@
                             </a>
                             @endif
 
-                            <x-primary-button class="ms-3">
-                                {{ __('Log in') }}
-                            </x-primary-button>
+                            <button type="submit" class="ms-3 bg-indigo-600 text-white px-4 py-2 rounded-md shadow hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                {{ __('Se connecter') }}
+                            </button>
                         </div>
                     </form>
                     <!-- End Form -->

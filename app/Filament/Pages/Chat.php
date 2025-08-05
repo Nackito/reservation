281cc54bbf3146a53aaf61429fb3dc2b71617e6d
Nackito <nackfrank@gmail.com>
2025-08-06 01:40:45 +0200

    Ajout de la page de chat avec l'icône de navigation et la vue associée.

---
+++ app/Filament/Pages/Chat.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class Chat extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-chat-bubble-left-right';
+
+    protected static string $view = 'filament.pages.chat';
+}

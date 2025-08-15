c168413623cfb6b57ddbf3e1afc47402d5d60a46
Nackito <nackfrank@gmail.com>
2025-08-16 00:58:22 +0200

    Ajout de la colonne 'prenom' dans UserResource et création de la migration pour ajouter la colonne 'phone' à la table 'users'.

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -83,6 +83,8 @@ class UserResource extends Resource
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
+                Tables\Columns\TextColumn::make('prenom')
+                    ->searchable(),
 
                 Tables\Columns\TextColumn::make('email')
                     ->searchable(),

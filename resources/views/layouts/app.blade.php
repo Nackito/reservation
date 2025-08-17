8ef394dd76a5892af57602d495d63b4f97ab5037
Nackito <nackfrank@gmail.com>
2025-08-17 18:22:26 +0200

    Amélioration de la balise <head> : ajout de métadonnées pour la compatibilité mobile et l'accessibilité.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,9 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- WebView & mobile friendly -->
+    <meta name="theme-color" content="#2563eb">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
 
     <title>{{ $title ?? 'Afridayz'}}</title>
 

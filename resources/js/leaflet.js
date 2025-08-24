fbdf3c9f2d69ab6032ed5c25d3722dc6a03b6253
Nackito <nackfrank@gmail.com>
2025-08-24 18:20:37 +0200

    Ajout de l'attribution pour les tuiles OpenStreetMap dans la fonction d'initialisation de la carte.

--- resources/js/leaflet.js
+++ resources/js/leaflet.js
@@ -2,6 +2,8 @@ function init() {
     const map = L.map("map").setView([7.54, -5.55], 7);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         maxZoom: 19,
+        attribution:
+            '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     }).addTo(map);
 }
 

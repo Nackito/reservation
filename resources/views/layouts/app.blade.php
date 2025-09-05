9b8fe62ac9d6ae407c322bad569cba87aa2460cc
Nackito <nackfrank@gmail.com>
2025-09-05 12:32:51 +0200

    Désactive l'inclusion du script Preline dans app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,7 +58,7 @@
 
 
     <!-- Inclure les scripts Preline -->
-    <script src="https://cdn.jsdelivr.net/npm/preline@1.0.0/dist/preline.min.js"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/preline@1.0.0/dist/preline.min.js"></script> -->
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>

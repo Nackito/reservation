2ed6a20db87bd66e9316e516019a6380fb61d18c
Nackito <nackfrank@gmail.com>
2025-09-06 20:26:42 +0200

    Supprime le lien vers la messagerie dans la barre de navigation

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -61,10 +61,6 @@
                         @endguest
                         @auth
 
-                        <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-blue-600 hover:bg-blue-50 focus:ring-2 focus:ring-blue-500 dark:text-blue-400 dark:hover:bg-gray-700 dark:hover:text-blue-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-blue-600 md:flex hidden" href="/chat">
-                            <i class="fas fa-comments mr-2"></i>Messagerie
-                        </a>
-
                         <a class="flex items-center gap-x-3.5 py-2 px-3 rounded-lg text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 md:flex hidden" href="{{ route('user-reservations') }}">
                             Mes réservations
                         </a>

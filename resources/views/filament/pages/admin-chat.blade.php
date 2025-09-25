a05aa32974efa0eb0e25577d46ada1dd9096e873
Nackito <nackfrank@gmail.com>
2025-09-25 15:44:44 +0200

    style: ajuste les commentaires et scoping CSS pour la page du chat admin

--- resources/views/filament/pages/admin-chat.blade.php
+++ resources/views/filament/pages/admin-chat.blade.php
@@ -1,9 +1,11 @@
 <x-filament-panels::page>
-  <x-slot name="header">
-    <h1 class="text-xl font-semibold text-gray-900 dark:text-white">
-      Conversations
-    </h1>
-  </x-slot>
+  <div class="admin-chat">
+    <x-slot name="header">
+      <h1 class="text-xl font-semibold text-gray-900 dark:text-white">
+        Conversations
+      </h1>
+    </x-slot>
 
-  <livewire:admin-chat-box />
+    <livewire:admin-chat-box />
+  </div>
 </x-filament-panels::page>

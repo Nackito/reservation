591890e50fa6d1d065536fd9fee70b6b2912cfbf
Nackito <nackfrank@gmail.com>
2025-09-25 13:52:50 +0200

    Ajoute un message d'information pour les utilisateurs non employés sur le lien de retour à la connexion

--- resources/views/partials/filament-login-back-link.blade.php
+++ resources/views/partials/filament-login-back-link.blade.php
@@ -1,5 +1,5 @@
 <div class="mt-4 text-center">
   <a href="{{ route('login') }}" class="text-primary-600 hover:underline">
-    ← Retour à la connexion utilisateur
+    ← Vous n'êtes pas employé ? Retour à la connexion utilisateur
   </a>
 </div>
